e9063564526bfe6e395870fbae097bd9f3d41630
Mishieck Mwale <mishieckdev@gmail.com>
2026-04-10 09:03:34 +0200

    feat: add zig to path

--- bashrc/variables.bash
+++ bashrc/variables.bash
@@ -2,4 +2,5 @@ export ANDROID_AVD_HOME="/home/mishieck-mwale/.config/.android/avd"
 export ANDROID_HOME="/home/mishieck-mwale/Android/Sdk"
 export EDITOR="helix"
 export JAVA_HOME="/usr/lib/jvm/java-21-openjdk-amd64"
-export PATH="$PATH:$ANDROID_AVD_HOME:$ANDROID_HOME:$BUN_INSTALL:$JAVA_HOME"
+export Zig=~/.zvm/bin
+export PATH="$PATH:$ANDROID_AVD_HOME:$ANDROID_HOME:$BUN_INSTALL:$JAVA_HOME:$Zig"
